f32eb761bbce04f892ba78b134c50c7260e97f01
Joseph Puglisi <157634624+JoePugs@users.noreply.github.com>
2024-09-22 16:56:32 -0400

    Submit button added to html, ingredient and brand storage to js

--- scripts.js
+++ scripts.js
@@ -1,11 +1,23 @@
+let ingredients = [];
+let selectedBrand = '';
+
+// button stuff
 document.getElementById('add-ingredient').addEventListener('click', function() {
     const ingredientInput = document.getElementById('ingredient');
     const ingredient = ingredientInput.value.trim();
 
+// brand button
+document.getElementById('brand-select').addEventListener('change', function() {
+    selectedBrand = this.value;
+
+});
+    // save ingredients in lsit
     if (ingredient) {
+        ingredients.push(ingredient);
         const li = document.createElement('li');
         li.textContent = ingredient;
         document.getElementById('ingredient-list').appendChild(li);
         ingredientInput.value = '';
     }
+
 });
